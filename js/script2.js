11ea7e8e54977b184bf9076efdd82ad4f7136e6f
jessikacostac <jessika.cost@gmail.com>
2018-06-18 11:12:36 -0300

    update v3

--- js/script2.js
+++ js/script2.js
@@ -22,123 +22,133 @@ var user = {
       name:"Squad Back-end"
     }
   ]
-};
+}
 
 // Acesso a DIV usuario
-var divUsuario = document.querySelector(".usuario");
+var AcessDiv = document.querySelector(".usuario")
 
 // Váriaveis dados--------------------------------------------------
-var nome = user.name;
-var email = user.email;
-var picture = './img/' + user.picture_url; //endereço da imagem
-var tagUm = (user.tags[0].name);
-var tagDois = (user.tags[1].name);
-var tagtres = (user.tags[2].name);
-var teamUm =(user.teams[0].name);
-var teamDois =(user.teams[1].name);
+var nome = user.name
+var email = user.email
+var picture = './img/' + user.picture_url //endereço da imagem
+var tagUm = (user.tags[0].name)
+var tagDois = (user.tags[1].name)
+var tagtres = (user.tags[2].name)
+var teamUm =(user.teams[0].name)
+var teamDois =(user.teams[1].name)
 
 //Formato Default-------------------------------------------------------------------------------------------------------------------------
-var defaultTeam = function () {
+var defaultT = function () {
   //Criar elementos-----------------------------------------------------
-  var criarDiv = document.createElement("div");
-  criarDiv.className = "user";
-  var tagImg = document.createElement("img");
-  tagImg.src = picture;
-  var tagH2 = document.createElement("h2");
-  var tagEmail = document.createElement("p");
-  var paragrafoTag1 = document.createElement("p");
-  var paragrafoTag2 = document.createElement("p");
-  var paragrafoTeam1 = document.createElement("p");
-  var paragrafoTeam2 = document.createElement("p");
-  var paragrafoVazio = document.createElement("p");
-  var form = document.createElement("form");
-  var tagInput = document.createElement("input");
-  tagInput.maxLength = 30;
-  tagInput.size = 30;
-  tagInput.value = "Digite seu status";
-  var divOptions = document.createElement("div");
+  var userDiv = document.createElement("div")
+  userDiv.classList.add("user")
+  var tagImg = document.createElement("img")
+  tagImg.src = picture
 
-  var tag = document.createElement("small");
-  var inputRadioTag = document.createElement("input");
-  inputRadioTag.type="radio";
-  inputRadioTag.id="tag";
-  var smallTeam = document.createElement("small");
-  var inputRadioTeam = document.createElement("input");
-  inputRadioTeam.type="radio";
-  inputRadioTeam.id="team";
-  inputRadioTeam.name="options";
+  var titleDiv = document.createElement("div")
+  var tagH2 = document.createElement("h2")
 
-  var smallTag = document.createElement("small");
-  var inputRadioTag = document.createElement("input");
-  inputRadioTag.type="radio";
-  inputRadioTag.id="tag";
-  inputRadioTag.name="options";
+  var infoDiv = document.createElement("div")
+  infoDiv.classList.add("info")
 
-  var smallEmail = document.createElement("small");
-  var inputRadioEmail = document.createElement("input");
-  inputRadioEmail.type="radio";
-  inputRadioEmail.id="email";
-  inputRadioEmail.name="options";
+  var tagEmail = document.createElement("p")
+  var pTag1 = document.createElement("p")
+  var pTag2 = document.createElement("p")
+  var pTeam1 = document.createElement("p")
+  var pTeam2 = document.createElement("p")
+  var pStatus = document.createElement("p")
+  var form = document.createElement("form")
+  var statusInput = document.createElement("input")
+  statusInput.maxLength = 30
+  statusInput.size = 30
+  //statusInput.value = "Digite seu status"
+
+  var optionsDiv = document.createElement("div")
+
+  var optionsA = document.createElement("a")
+
+  // var tag = document.createElement("small")
+  // var inputRadioTag = document.createElement("input")
+  // inputRadioTag.type="radio"
+  // inputRadioTag.id="tag"
+  // var smallTeam = document.createElement("small")
+  // var inputRadioTeam = document.createElement("input")
+  // inputRadioTeam.type="radio"
+  // inputRadioTeam.id="team"
+  // inputRadioTeam.name="options"
+
+  // var smallTag = document.createElement("small")
+  // var inputRadioTag = document.createElement("input")
+  // inputRadioTag.type="radio"
+  // inputRadioTag.id="tag"
+  // inputRadioTag.name="options"
+
+  // var smallEmail = document.createElement("small")
+  // var inputRadioEmail = document.createElement("input")
+  // inputRadioEmail.type="radio"
+  // inputRadioEmail.id="email"
+  // inputRadioEmail.name="options"
   
   //Criar nós-----  Valores do objeto   -------------------------------
-  var imagem = document.createTextNode(picture);
-  var titulo = document.createTextNode(nome);
-  var textoEmail = document.createTextNode(email);
-  var tag1 = document.createTextNode(tagUm);
-  var tag2 = document.createTextNode(tagDois);
-  var team1 = document.createTextNode(teamUm);
-  var team2 = document.createTextNode(teamDois);
-  var textoTeam = document.createTextNode("Team");
-  var textoTag = document.createTextNode("Tag");
-  var textoEmail = document.createTextNode("E-mail");
+  var imagem = document.createTextNode(picture)
+  var titulo = document.createTextNode(nome)
+  var textoEmail = document.createTextNode(email)
+  var tag1 = document.createTextNode(tagUm)
+  var tag2 = document.createTextNode(tagDois)
+  var team1 = document.createTextNode(teamUm)
+  var team2 = document.createTextNode(teamDois)
+  
+  // var textoTeam = document.createTextNode("Team")
+  // var textoTag = document.createTextNode("Tag")
+  // var textoEmail = document.createTextNode("E-mail")
 
 
   //Vincular nós ao elementos--------------------------------------------
-  tagImg.appendChild(imagem);
-  tagH2.appendChild(titulo);
-  tagEmail.appendChild(textoEmail);
-  paragrafoTag1.appendChild(tag1);
-  paragrafoTag2.appendChild(tag2);
-  paragrafoTeam1.appendChild(team1);
-  paragrafoTeam2.appendChild(team2);
-  smallTeam.appendChild(textoTeam);
-  smallTag.appendChild(textoTag);
-  smallEmail.appendChild(textoEmail);
+  tagImg.appendChild(imagem)
+  tagH2.appendChild(titulo)
+  tagEmail.appendChild(textoEmail)
+  pTag1.appendChild(tag1)
+  pTag2.appendChild(tag2)
+  pTeam1.appendChild(team1)
+  pTeam2.appendChild(team2)
+  // smallTeam.appendChild(textoTeam)
+  // smallTag.appendChild(textoTag)
+  // smallEmail.appendChild(textoEmail)
 
 
   //Vincular elementos ao documento e a div pai---------------------------------------
   //------------------------- Padrão TAG -------------------------------------
   if (teamUm !== undefined & teamDois !== undefined) {
-    divUsuario.appendChild(criarDiv);
-    criarDiv.appendChild(tagImg);//foto
-    criarDiv.appendChild(tagH2);//nome
-    criarDiv.appendChild(paragrafoTeam1); //team1
-    criarDiv.appendChild(paragrafoTeam2);//team2
-    criarDiv.appendChild(form);
-    form.appendChild(tagInput);//campo texto
-    criarDiv.appendChild(paragrafoVazio); //paragrafo para receber texto
-   // document.getElementById("escolha").classList.remove("apagar");
-    criarDiv.appendChild(divOptions);
-    divOptions.appendChild(inputRadioTeam);
-    divOptions.appendChild(smallTeam);
-    divOptions.appendChild(inputRadioTag);
-    divOptions.appendChild(smallTag);
-    divOptions.appendChild(inputRadioEmail);
-    divOptions.appendChild(smallEmail);
+    AcessDiv.appendChild(userDiv)
+    userDiv.appendChild(tagImg)//foto
+    userDiv.appendChild(tagH2)//nome
+    userDiv.appendChild(pTeam1) //team1
+    userDiv.appendChild(pTeam2)//team2
+    userDiv.appendChild(form)
+    form.appendChild(statusInput)//campo texto
+    userDiv.appendChild(pStatus) //paragrafo para receber texto
+   // document.getElementById("escolha").classList.remove("apagar")
+    userDiv.appendChild(optionsDiv)
+    // optionsDiv.appendChild(inputRadioTeam)
+    // optionsDiv.appendChild(smallTeam)
+    // optionsDiv.appendChild(inputRadioTag)
+    // optionsDiv.appendChild(smallTag)
+    // optionsDiv.appendChild(inputRadioEmail)
+    // optionsDiv.appendChild(smallEmail)
 
-    console.log("esta funcionando");
+    console.log("esta funcionando")
   } 
   else if (teamUm !== undefined & teamDois === undefined) {
-    divUsuario.appendChild(criarDiv);
-    criarDiv.appendChild(tagImg);//foto
-    criarDiv.appendChild(tagH2);//nome
-    criarDiv.appendChild(paragrafoTeam1); //team1
-    criarDiv.appendChild(form);
-    form.appendChild(tagInput);//campo texto
-    criarDiv.appendChild(paragrafoVazio); //paragrafo para receber texto
-    //document.getElementById("escolha").classList.remove("apagar");
-    criarDiv.appendChild(divOptions);
-    divOptions.appendChild(inputRadioTeam);
+    AcessDiv.appendChild(userDiv)
+    userDiv.appendChild(tagImg)//foto
+    userDiv.appendChild(tagH2)//nome
+    userDiv.appendChild(pTeam1) //team1
+    userDiv.appendChild(form)
+    form.appendChild(statusInput)//campo texto
+    userDiv.appendChild(pStatus) //paragrafo para receber texto
+    //document.getElementById("escolha").classList.remove("apagar")
+    userDiv.appendChild(optionsDiv)
+    optionsDiv.appendChild(inputRadioTeam)
 
   }
 
@@ -146,56 +156,56 @@ var defaultTeam = function () {
 
   if (tagUm !== undefined & tagDois !== undefined & teamUm === undefined & teamDois === undefined ) {
       //Vincular elementos ao documento e a div mae---------------------------------------
-      divUsuario.appendChild(criarDiv);
-      criarDiv.appendChild(tagImg);//foto
-      criarDiv.appendChild(tagH2);//nome
-      criarDiv.appendChild(paragrafoTag1); //tag1
-      criarDiv.appendChild(paragrafoTag2); //tag2
-      criarDiv.appendChild(form);
-      form.appendChild(tagInput);//campo texto
-      criarDiv.appendChild(paragrafoVazio); //paragrafo para receber texto
-      //document.getElementById("escolha").classList.remove("apagar");
+      AcessDiv.appendChild(userDiv)
+      userDiv.appendChild(tagImg)//foto
+      userDiv.appendChild(tagH2)//nome
+      userDiv.appendChild(pTag1) //tag1
+      userDiv.appendChild(pTag2) //tag2
+      userDiv.appendChild(form)
+      form.appendChild(statusInput)//campo texto
+      userDiv.appendChild(pStatus) //paragrafo para receber texto
+      //document.getElementById("escolha").classList.remove("apagar")
 
     } 
     else if (tagUm !== undefined & tagDois === undefined & teamUm === undefined & teamDois === undefined){
-      divUsuario.appendChild(criarDiv);
-      criarDiv.appendChild(tagImg);//foto
-      criarDiv.appendChild(tagH2);//nome
-      criarDiv.appendChild(paragrafoTag1); //tag1
-      criarDiv.appendChild(form);
-      form.appendChild(tagInput);//campo texto
-      criarDiv.appendChild(paragrafoVazio); //paragrafo para receber texto
-      //document.getElementById("escolha").classList.remove("apagar");
+      AcessDiv.appendChild(userDiv)
+      userDiv.appendChild(tagImg)//foto
+      userDiv.appendChild(tagH2)//nome
+      userDiv.appendChild(pTag1) //tag1
+      userDiv.appendChild(form)
+      form.appendChild(statusInput)//campo texto
+      userDiv.appendChild(pStatus) //paragrafo para receber texto
+      //document.getElementById("escolha").classList.remove("apagar")
 
     }
 
       //------------------------- Padrão Email -------------------------------------
 
     if(email !== undefined & tagUm === undefined & tagDois === undefined & teamUm === undefined & teamDois === undefined) {
-      divUsuario.appendChild(criarDiv);
-      criarDiv.appendChild(tagImg);//foto
-      criarDiv.appendChild(tagH2);//nome
-      criarDiv.appendChild(tagEmail);
-      criarDiv.appendChild(form);
-      form.appendChild(tagInput);//campo texto
-      criarDiv.appendChild(paragrafoVazio); //paragrafo para receber texto
+      AcessDiv.appendChild(userDiv)
+      userDiv.appendChild(tagImg)//foto
+      userDiv.appendChild(tagH2)//nome
+      userDiv.appendChild(tagEmail)
+      userDiv.appendChild(form)
+      form.appendChild(statusInput)//campo texto
+      userDiv.appendChild(pStatus) //paragrafo para receber texto
     }
 
     // 3) Override---------------------------------------------------------------------------
     ///////função para apagar ou evento itens 
 
         // 4)Texto customizável-----------------------------------------------------------------
-        inputRadioTag.addEventListener("checked", function (event) {
-          event.preventDefault();
-          //Remove os elementos selecionados ***********
-          document.querySelector('.user p').remove()
-          //////Cria um novo Elementooooo
+        // inputRadioTag.addEventListener("checked", function (event) {
+        //   event.preventDefault()
+        //   //Remove os elementos selecionados ***********
+        //   document.querySelector('.user p').remove()
+        //   //////Cria um novo Elementooooo
 
-          console.log("funcionouuuuuuuu")
+        //   console.log("funcionouuuuuuuu")
 
 
 
-        });
+        // })
 
 
 
@@ -224,45 +234,45 @@ var defaultTeam = function () {
         var todoList = document.getElementById("list")
         // Colocar novo li dentro da lista
         todoList.appendChild(newTodoItem)
-        todoInput.classList="apagar";
+        todoInput.classList="apagar"
       }
-    });
+    })
     //FIM Texto customizável-----------------------------------------------------------------
 }
 
 //Formato Small-----------------------------------------------------------
 var small = function(){
   //Criar elementos-----------------------------------------------------
-  var criarDiv = document.createElement("div");
-  var tagH2 = document.createElement("h2");
+  var userDiv = document.createElement("div")
+  var tagH2 = document.createElement("h2")
   // Aplicar estilo ------------------------------------------------------
-  criarDiv.className = "usuarios";
+  userDiv.className = "usuarios"
   //Criar nós-----  Valores do objeto   -------------------------------
-  var titulo = document.createTextNode(nome);
+  var titulo = document.createTextNode(nome)
   //Vincular nós ao elementos--------------------------------------------
-  tagH2.appendChild(titulo);
+  tagH2.appendChild(titulo)
 //Vincular elementos ao documento e a div mae---------------------------------------
-  divUsuario.appendChild(criarDiv);
-  criarDiv.appendChild(tagH2);
-};
+  AcessDiv.appendChild(userDiv)
+  userDiv.appendChild(tagH2)
+}
 
 //Formato Only-Picture-----------------------------------------------------------
 var onlyPicture = function(){
   //Criar elementos-----------------------------------------------------
-  var criarDiv = document.createElement("div");
-  var tagImg = document.createElement("img");
+  var userDiv = document.createElement("div")
+  var tagImg = document.createElement("img")
   // Aplicar estilo ------------------------------------------------------
-  criarDiv.className = "usuarios";
+  userDiv.className = "usuarios"
   // Aplicar src da picture na img----------------------------------------
-  tagImg.src = picture;
+  tagImg.src = picture
   //Criar nós-----  Valores do objeto   -------------------------------
-  var imagem = document.createTextNode(picture);
+  var imagem = document.createTextNode(picture)
   //Vincular nós ao elementos--------------------------------------------
-  tagImg.appendChild(imagem);
+  tagImg.appendChild(imagem)
 //Vincular elementos ao documento e a div mae---------------------------------------
-  divUsuario.appendChild(criarDiv);
-  criarDiv.appendChild(tagImg);//foto
-};
+  AcessDiv.appendChild(userDiv)
+  userDiv.appendChild(tagImg)//foto
+}
 
 
 
